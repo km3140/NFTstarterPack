6a039efe5a05c573e7f2b9387379a0bb8e7d1f53
km3140 <68173931+km3140@users.noreply.github.com>
2023-06-18 19:43:50 +0900

    Update 32_에러핸들러의 작동방식.sol

--- 32_에러핸들러의 작동방식.sol
+++ 32_에러핸들러의 작동방식.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.17;
 
 contract ABC{
 
-  //정확하지 않음
-
-  // CALL이나 델리게이트CALL 을 제외하고는 에러핸들러는 해당 범위 전체를 원상태로 되돌린다? (event emit이 되지 않은 이유?)
+  // 에러핸들러는 해당 범위 전체를 원상태로 되돌린다? (event emit이 되지 않은 이유?)
   // 에러핸들러가 있는 블록에 진입하면 위 코드가 어떻든 에러핸들러가 무조건 발동됨? 
 
   event afterUnderbar(string _message);
